89e24391637f22cdc9a17965029c826b4d9c09c0
devaaditya28 <devaaditya28@gmail.com>
2023-02-22 13:59:10 +0700

    view data dummy mata kuliah

--- resources/views/v_home.blade.php
+++ resources/views/v_home.blade.php
@@ -1,7 +1,8 @@
 @extends('temp.v_temp')
 @section('title', (''))
 @section('isicontent')
-
+<link rel="stylesheet" href="https://kit.fontawesome.com/d4d93077ce.css" crossorigin="anonymous">
+<script src="https://kit.fontawesome.com/d4d93077ce.js" crossorigin="anonymous"></script>
 <style>
 ul.breadcrumb {
   padding: 10px 16px;
@@ -50,7 +51,127 @@ ul.breadcrumb li a:hover {
                                     <th>Action</th>
                                 </tr>
                             </thead>
+
                             <tbody>
+                            <thead>
+                              <tr>
+                                <td>1</td>
+                                <td>2013</td>
+                                <td>IF1101</td>
+                                <td>Fisika 1</td>
+                                <td>2</td>
+                                <td>Teori</td>
+                                <td>
+                                  <i class="fa-solid fa-eye"></i>
+                                  <i class="fa-solid fa-pencil"></i>
+                                </td>
+                              </tr>
+                            </thead>
+
+                            <thead>
+                              <tr>
+                                <td>2</td>
+                                <td>2013</td>
+                                <td>IF1102</td>
+                                <td>Pengantar Teknik Informatika</td>
+                                <td>2</td>
+                                <td>Teori</td>
+                                <td>
+                                  <i class="fa-solid fa-eye"></i>
+                                  <i class="fa-solid fa-pencil"></i>
+                                </td>
+                              </tr>
+                            </thead>
+
+                            <thead>
+                              <tr>
+                                <td>3</td>
+                                <td>2013</td>
+                                <td>KD1101</td>
+                                <td>Algoritma</td>
+                                <td>3</td>
+                                <td>Teori</td>
+                                <td>
+                                  <i class="fa-solid fa-eye"></i>
+                                  <i class="fa-solid fa-pencil"></i>
+                                </td>
+                              </tr>
+                            </thead>
+
+                            <thead>
+                              <tr>
+                                <td>4</td>
+                                <td>2013</td>
+                                <td>KD1102</td>
+                                <td>Kalkulus</td>
+                                <td>3</td>
+                                <td>Teori</td>
+                                <td>
+                                  <i class="fa-solid fa-eye"></i>
+                                  <i class="fa-solid fa-pencil"></i>
+                                </td>
+                              </tr>
+                            </thead>
+
+                            <thead>
+                              <tr>
+                                <td>5</td>
+                                <td>2013</td>
+                                <td>KD1103</td>
+                                <td>Paket Aplikasi</td>
+                                <td>2</td>
+                                <td>Teori dan Praktek</td>
+                                <td>
+                                  <i class="fa-solid fa-eye"></i>
+                                  <i class="fa-solid fa-pencil"></i>
+                                </td>
+                              </tr>
+                            </thead>
+
+                            <thead>
+                              <tr>
+                                <td>6</td>
+                                <td>2013</td>
+                                <td>KD1104</td>
+                                <td>Pemrograman 1</td>
+                                <td>3</td>
+                                <td>Teori dan Praktek</td>
+                                <td>
+                                  <i class="fa-solid fa-eye"></i>
+                                  <i class="fa-solid fa-pencil"></i>
+                                </td>
+                              </tr>
+                            </thead>
+
+                            <thead>
+                              <tr>
+                                <td>7</td>
+                                <td>2013</td>
+                                <td>KU1101</td>
+                                <td>Bahasa Inggris 1</td>
+                                <td>2</td>
+                                <td>Teori</td>
+                                <td>
+                                  <i class="fa-solid fa-eye"></i>
+                                  <i class="fa-solid fa-pencil"></i>
+                                </td>
+                              </tr>
+                            </thead>
+
+                            <thead>
+                              <tr>
+                                <td>8</td>
+                                <td>2013</td>
+                                <td>KU1102</td>
+                                <td>Manajemen dan Perilaku Organisasi</td>
+                                <td>2</td>
+                                <td>Teori</td>
+                                <td>
+                                  <i class="fa-solid fa-eye"></i>
+                                  <i class="fa-solid fa-pencil"></i>
+                                </td>
+                              </tr>
+                            </thead>
 
                             </tbody>
                         </table>

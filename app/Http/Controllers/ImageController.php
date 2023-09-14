7f29a8d5949302f8ad0b954eb7695e7d136051db
Muhamad Ardhan Hadaina <65588203+ardhanshahab@users.noreply.github.com>
2023-09-14 08:19:06 -0700

    Prasidang finalisasi Aplikasi

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,23 +4,56 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\profileImage;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
-    public function store(Request $request)
+    public function fotoprofil(Request $request)
     {
         $this->validate($request, [
-            'image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+            'image'     => 'required|mimes:jpg,png|max:4048',
+            'keterangan' => 'required',
+            'nim'   => 'required'
         ]);
+        //get data Blog by ID
+        $blog = profileImage::findOrFail($request->nim);
 
-        $image_path = $request->file('image')->store('image', 'public');
+        if($request->file('link') == "") {
+            // upload image
+            // $nama = $request->kode_mk;
+        $nama = $request->nim;
+        $image = $request->file('image');
+        // $request->file('file')->store('public/files');
+        $image->store('public/data_file', $nama );
 
-        $data = profileImage::create([
-            'image' => $image_path,
+        $blog = profileImage::create([
+            'image'     => $nama,
+            'keterangan' => $request->keterangan,
+            'nim'   => $request->nim
         ]);
+        } else {
+            //hapus old image
+            Storage::disk('local')->delete('public/data_file/'.$blog->image);
 
-        session()->flash('success', 'Image Upload successfully');
+            //upload new image
+            $image = $request->file('image');
+            $image->storeAs('public/data_file',$request->nim);
 
-        return redirect()->route('image.mahasiswa.myprofile');
+            $blog->update([
+                'image'     => $request->nim,
+                'keterangan' => $request->keterangan,
+                'kd_mk'   => $request->nim
+            ]);
+
+        }
+        if($blog){
+            //redirect dengan pesan sukses
+            return redirect()->back()->with(['success' => 'Data Berhasil Disimpan!']);
+        }else{
+            //redirect dengan pesan error
+            return redirect()->back()->with(['error' => 'Data Gagal Disimpan!']);
+        }
+
+        // return redirect()->back()->with('status', 'File Has been uploaded successfully');
     }
 }
